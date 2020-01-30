e031308181d210c5e608c4506f495e594f3206bc
Mirko De Grandis <degami@gmail.com>
2020-01-30 22:29:31 +0100

    fixes

--- app/base/traits/ToolsTrait.php
+++ app/base/traits/ToolsTrait.php
@@ -12,12 +12,15 @@
 namespace App\Base\Traits;
 
 use \Exception;
+use \Degami\Basics\Traits\ToolsTrait as BasicToolsTrait;
 
 /**
  * utils Trait
  */
 trait ToolsTrait
 {
+    use BasicToolsTrait;
+
     /**
      * executes an http request
      *
@@ -52,80 +55,5 @@ trait ToolsTrait
             return false;
         }
         throw new Exception("Error. '".get_class($this)."' is not a ContainerAwareObject", 1);
-    }
-
-    /**
-     * Take a string_like_this and return a StringLikeThis
-     *
-     * @param  string
-     * @return string
-     */
-    public static function snakeCaseToPascalCase($input)
-    {
-        return str_replace(' ', '', ucwords(str_replace('_', ' ', $input)));
-    }
-
-    /**
-     * Take a StringLikeThis and return string_like_this
-     *
-     * @param  string
-     * @return string
-     */
-    public static function pascalCaseToSnakeCase($input)
-    {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-        return implode('_', $ret);
-    }
-
-    /**
-     * slugify string
-     *
-     * @param  string $text
-     * @return string
-     */
-    public function slugify($text)
-    {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\pL\d\/]+~u', '-', $text);
-
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w\/]+~', '', $text);
-
-        // trim
-        $text = trim($text, '-');
-
-        // remove duplicate -
-        $text = preg_replace('~-+~', '-', $text);
-
-        // lowercase
-        $text = strtolower($text);
-
-        if (empty($text)) {
-            return 'n-a';
-        }
-
-        return $text;
-    }
-
-    /**
-     * format byte size
-     *
-     * @param  integer $size size in bytes
-     * @return string       formatted size
-     */
-    public static function formatBytes($size)
-    {
-        $units = [' B', ' KB', ' MB', ' GB', ' TB'];
-        for ($i = 0; $size >= 1024 && $i < 4; $i++) {
-            $size /= 1024;
-        }
-        return round($size, 2).$units[$i];
     }
 }

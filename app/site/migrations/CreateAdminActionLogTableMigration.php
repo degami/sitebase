43e87ed2ebef058077e151302bd6e73e7f1d6b68
Mirko De Grandis <degami@gmail.com>
2020-03-01 15:15:36 +0100

    added admin action logs, improved admin page, creating a new websites now copies system configuration from website 1

---
+++ app/site/migrations/CreateAdminActionLogTableMigration.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * SiteBase
+ * PHP Version 7.0
+ *
+ * @category CMS / Framework
+ * @package  Degami\Sitebase
+ * @author   Mirko De Grandis <degami@github.com>
+ * @license  MIT https://opensource.org/licenses/mit-license.php
+ * @link     https://github.com/degami/sitebase
+ */
+namespace App\Site\Migrations;
+
+use \App\Base\Abstracts\DBMigration;
+use \Psr\Container\ContainerInterface;
+use \Degami\SqlSchema\Index;
+use \Degami\SqlSchema\Table;
+
+/**
+ * "admin_action_log" table migration
+ */
+class CreateAdminActionLogTableMigration extends DBMigration
+{
+    /**
+     * @var string table name
+     */
+    protected $tableName = 'admin_action_log';
+
+    /**
+     * {@inheritdocs}
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return '06_'.parent::getName();
+    }
+
+    /**
+     * {@inheritdocs}
+     *
+     * @param  Table $table
+     * @return Table
+     */
+    public function addDBTableDefinition(Table $table)
+    {
+        $table->addColumn('id', 'INT', null, ['UNSIGNED'], false)
+            ->addColumn('website_id', 'INT', null, ['UNSIGNED'])
+            ->addColumn('url', 'VARCHAR', [1024])
+            ->addColumn('method', 'VARCHAR', [10])
+            ->addColumn('ip_address', 'VARCHAR', [32])
+            ->addColumn('user_id', 'INT', null, ['UNSIGNED'], 'NULL')
+            ->addColumn('action', 'VARCHAR', [1024])
+            ->addColumn('route_info', 'TEXT')
+            ->addColumn('log_data', 'TEXT')
+            ->addColumn('created_at', 'TIMESTAMP', null, [], false, 'CURRENT_TIMESTAMP()')
+            ->addColumn('updated_at', 'TIMESTAMP', null, [], false, 'CURRENT_TIMESTAMP()')
+            ->addIndex(null, 'id', Index::TYPE_PRIMARY)
+            ->addForeignKey('fk_adminactionlog_website_id', ['website_id'], 'website', ['id'])
+            ->addForeignKey('fk_adminactionlog_user_id', ['user_id'], 'user', ['id'])
+            ->setAutoIncrementColumn('id');
+
+        return $table;
+    }
+}
---
+++ app/site/models/AdminActionLog.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * SiteBase
+ * PHP Version 7.0
+ *
+ * @category CMS / Framework
+ * @package  Degami\Sitebase
+ * @author   Mirko De Grandis <degami@github.com>
+ * @license  MIT https://opensource.org/licenses/mit-license.php
+ * @link     https://github.com/degami/sitebase
+ */
+namespace App\Site\Models;
+
+use \App\Site\Routing\RouteInfo;
+use \App\Base\Abstracts\BasePage;
+use \Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Admin Action Log Model
+ *
+ * @method int getId()
+ * @method int getWebsiteId()
+ * @method string getUrl()
+ * @method string getMethod()
+ * @method string getUserId()
+ * @method string getIpAddress()
+ * @method RouteInfo getRouteInfo()
+ * @method mixed getLogData()
+ * @method \DateTime getCreatedAt()
+ * @method \DateTime getUpdatedAt()
+ */
+class AdminActionLog extends RequestLog
+{
+    /**
+     * fills log with request object
+     *
+     * @param  Request           $request
+     * @param  BaseHtmlPage|null $controller
+     * @return self
+     */
+    public function fillWithRequest(Request $request, BasePage $controller = null)
+    {
+        parent::fillWithRequest($request, $controller);
+        $this->setRouteInfo(serialize($controller->getRouteInfo()));
+        $this->setAction($controller->getRouteInfo()->getRouteName());
+        return $this;
+    }
+
+    public function getRouteInfo()
+    {
+        $route_info = unserialize($this->getData('route_info'));
+        return $route_info;
+    }
+
+    public function getLogData()
+    {
+        $log_data = $this->getData('log_data');
+        var_dump($log_data);
+    }
+}

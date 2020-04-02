020475611e3c4ee0a2ecd3e9d4c14960ce6bd207
Mirko De Grandis <degami@gmail.com>
2020-04-02 22:59:52 +0200

    better website configuration copy

--- app/site/models/Website.php
+++ app/site/models/Website.php
@@ -43,7 +43,8 @@ class Website extends BaseModel
     public function postPersist()
     {
         if ($this->isFirstSave()) {
-            $copy_from = 1;
+            $first_website = $this->getContainer()->call([Website::class,'select'], ['options' => ['expr' => 'id', 'limitCount' => 1]])->fetch();
+            $copy_from = $first_website['id'];
             $copy_configurations = $this->getContainer()->call([Configuration::class, 'where'], ['condition' => ['is_system' => 1, 'website_id' => $copy_from]]);
             $configurations = [];
             foreach ($copy_configurations as $to_copy) {

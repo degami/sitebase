96f84a412d2dd4495a8eb63cf1c4214d02834217
Mirko De Grandis <degami@gmail.com>
2020-05-17 11:29:07 +0200

    admin sidemenu

--- templates/admin/partials/sidemenu.php
+++ templates/admin/partials/sidemenu.php
@@ -1,169 +1,152 @@
 <ul class="nav flex-column">
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.dashboard');?>">
-            <?php $this->sitebase()->drawIcon('home'); ?> <span class="text"><?= $this->sitebase()->translate('Dashboard');?></span>
-        </a>
-    </li>
 
-    <?php if ($controller->checkPermission('administer_blocks')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.blocks');?>">
-            <?php $this->sitebase()->drawIcon('box'); ?> <span class="text"><?= $this->sitebase()->translate('Blocks');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
+<?php
+$links = [
+    [
+        'permission_name' => '',
+        'route_name' => 'admin.dashboard',
+        'icon' => 'home',
+        'text' => 'Dashboard',
+    ],
+    [
+        'permission_name' => 'administer_blocks',
+        'route_name' => 'admin.blocks',
+        'icon' => 'box',
+        'text' => 'Blocks',
+    ],
+    [
+        'permission_name' => 'administer_pages',
+        'route_name' => 'admin.pages',
+        'icon' => 'file-text',
+        'text' => 'Pages',
+    ],
+    [
+        'permission_name' => 'administer_news',
+        'route_name' => 'admin.news',
+        'icon' => 'file-text',
+        'text' => 'News',
+    ],
+    [
+        'permission_name' => 'administer_taxonomy',
+        'route_name' => 'admin.taxonomy',
+        'icon' => 'list',
+        'text' => 'Taxonomy',
+    ],
+    [
+        'permission_name' => 'administer_medias',
+        'route_name' => 'admin.media',
+        'icon' => 'image',
+        'text' => 'Media',
+    ],
+    [
+        'permission_name' => 'administer_medias',
+        'route_name' => 'admin.mediarewrites',
+        'icon' => 'layers',
+        'text' => 'Rewrites Media',
+    ],
+    [
+        'permission_name' => 'administer_contact',
+        'route_name' => 'admin.contactforms',
+        'icon' => 'file-text',
+        'text' => 'Contact Forms',
+    ],
+    [
+        'permission_name' => 'administer_links',
+        'route_name' => 'admin.links',
+        'icon' => 'link',
+        'text' => 'Links',
+    ],
+    [
+        'permission_name' => 'administer_menu',
+        'route_name' => 'admin.menus',
+        'icon' => 'menu',
+        'text' => 'Menu',
+    ],
+    [
+        'permission_name' => 'administer_languages',
+        'route_name' => 'admin.languages',
+        'icon' => 'flag',
+        'text' => 'Languages',
+    ],
+    [
+        'permission_name' => 'administer_users',
+        'route_name' => 'admin.roles',
+        'icon' => 'award',
+        'text' => 'Roles',
+    ],
+    [
+        'permission_name' => 'administer_permissions',
+        'route_name' => 'admin.permissions',
+        'icon' => 'star',
+        'text' => 'Permissions',
+    ],
+    [
+        'permission_name' => 'administer_users',
+        'route_name' => 'admin.users',
+        'icon' => 'user',
+        'text' => 'Users',
+    ],
+    [
+        'permission_name' => 'administer_rewrites',
+        'route_name' => 'admin.rewrites',
+        'icon' => 'globe',
+        'text' => 'Rewrites',
+    ],
+    [
+        'permission_name' => 'administer_sitemaps',
+        'route_name' => 'admin.sitemaps',
+        'icon' => 'link',
+        'text' => 'Sitemaps',
+    ],
+    [
+        'permission_name' => 'administer_logs',
+        'route_name' => 'admin.logs',
+        'icon' => 'info',
+        'text' => 'Logs',
+    ],
+    [
+        'permission_name' => 'administer_site',
+        'route_name' => 'admin.config',
+        'icon' => 'sliders',
+        'text' => 'Config',
+    ],
+    [
+        'permission_name' => 'administer_site',
+        'route_name' => 'admin.websites',
+        'icon' => 'globe',
+        'text' => 'Websites',
+    ],
+    [
+        'permission_name' => 'administer_cron',
+        'route_name' => 'admin.cron',
+        'icon' => 'watch',
+        'text' => 'Cron Tasks',
+    ],
+    [
+        'permission_name' => 'administer_queue',
+        'route_name' => 'admin.queue',
+        'icon' => 'truck',
+        'text' => 'Queue',
+    ],
+//    [
+//        'permission_name' => '',
+//        'route_name' => '',
+//        'icon' => '',
+//        'text' => '',
+//    ],
+];
 
-    <?php if ($controller->checkPermission('administer_pages')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.pages');?>">
-            <?php $this->sitebase()->drawIcon('file-text'); ?> <span class="text"><?= $this->sitebase()->translate('Pages');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
+?>
 
-    <?php if ($controller->checkPermission('administer_news')) : ?>
+<?php foreach ($links as $key => $link) :?>
+    <?php if (empty($link['permission_name']) || $controller->checkPermission($link['permission_name'])) : ?>
     <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.news');?>">
-            <?php $this->sitebase()->drawIcon('file-text'); ?> <span class="text"><?= $this->sitebase()->translate('News');?></span>
+        <a class="nav-link<?= ($controller->getRouteName() == $link['route_name']) ? ' active' : '';?>" href="<?= $this->sitebase()->getUrl($link['route_name']);?>">
+            <?php $this->sitebase()->drawIcon($link['icon']); ?> <span class="text"><?= $this->sitebase()->translate($link['text']);?></span>
         </a>
     </li>
     <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_taxonomy')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.taxonomy');?>">
-            <?php $this->sitebase()->drawIcon('list'); ?> <span class="text"><?= $this->sitebase()->translate('Taxonomy');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_medias')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.media');?>">
-            <?php $this->sitebase()->drawIcon('image'); ?> <span class="text"><?= $this->sitebase()->translate('Media');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_medias')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.mediarewrites');?>">
-            <?php $this->sitebase()->drawIcon('layers'); ?> <span class="text"><?= $this->sitebase()->translate('Rewrites Media');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_contact')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.contactforms');?>">
-            <?php $this->sitebase()->drawIcon('file-text'); ?> <span class="text"><?= $this->sitebase()->translate('Contact Forms');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_links')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.links');?>">
-            <?php $this->sitebase()->drawIcon('link'); ?> <span class="text"><?= $this->sitebase()->translate('Links');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_menu')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.menus');?>">
-            <?php $this->sitebase()->drawIcon('menu'); ?> <span class="text"><?= $this->sitebase()->translate('Menu');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_languages')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.languages');?>">
-            <?php $this->sitebase()->drawIcon('flag'); ?> <span class="text"><?= $this->sitebase()->translate('Languages');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_users')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $controller->getUrl('admin.roles');?>">
-            <?php $this->sitebase()->drawIcon('award'); ?> <span class="text"><?= $this->sitebase()->translate('Roles');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_permissions')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.permissions');?>">
-            <?php $this->sitebase()->drawIcon('star'); ?> <span class="text"><?= $this->sitebase()->translate('Permissions');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_users')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.users');?>">
-            <?php $this->sitebase()->drawIcon('user'); ?> <span class="text"><?= $this->sitebase()->translate('Users');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_rewrites')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.rewrites');?>">
-            <?php $this->sitebase()->drawIcon('globe'); ?> <span class="text"><?= $this->sitebase()->translate('Rewrites');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_sitemaps')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.sitemaps');?>">
-            <?php $this->sitebase()->drawIcon('link'); ?> <span class="text"><?= $this->sitebase()->translate('Sitemaps');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_logs')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.logs');?>">
-            <?php $this->sitebase()->drawIcon('info'); ?> <span class="text"><?= $this->sitebase()->translate('Logs');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_site')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.config');?>">
-            <?php $this->sitebase()->drawIcon('sliders'); ?> <span class="text"><?= $this->sitebase()->translate('Config');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_site')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.websites');?>">
-            <?php $this->sitebase()->drawIcon('globe'); ?> <span class="text"><?= $this->sitebase()->translate('Websites');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if ($controller->checkPermission('administer_cron')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.cron');?>">
-            <?php $this->sitebase()->drawIcon('watch'); ?> <span class="text"><?= $this->sitebase()->translate('Cron Tasks');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if ($controller->checkPermission('administer_queue')) : ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?= $this->sitebase()->getUrl('admin.queue');?>">
-            <?php $this->sitebase()->drawIcon('truck'); ?> <span class="text"><?= $this->sitebase()->translate('Queue');?></span>
-        </a>
-    </li>
-    <?php endif; ?>
+<?php endforeach;?>
 
 
     <li>

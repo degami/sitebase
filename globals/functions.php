b3fa8479196651c469283cc5614b33ba261482c6
Mirko De Grandis <degami@gmail.com>
2020-09-06 22:17:23 +0200

    some utility functions

--- globals/functions.php
+++ globals/functions.php
@@ -2,6 +2,7 @@
 
 /**
  * translate string
+ *
  * @param  string $string
  * @param  string $locale
  * @return string
@@ -17,9 +18,32 @@ function __($string, $locale = null)
     return $app->getUtils()->translate($string, $locale);
 }
 
+/**
+ * utility function dump variable
+ *
+ * @param $variable
+ * @param string $level
+ */
 function k($variable, $level = 'debug')
 {
     global $app;
 
     $app->getContainer()->get('debugbar')['messages']->log($level, $variable);
+}
+
+/**
+ * utility function to quickly execute a statement
+ *
+ * @param $query_string
+ * @param null $params
+ * @return bool|PDOStatement
+ */
+function dbq($query_string, $params = null)
+{
+    global $app;
+
+    $stmt = $app->getContainer()->get('pdo')->prepare($query_string);
+    $stmt->execute($params);
+
+    return $stmt;
 }

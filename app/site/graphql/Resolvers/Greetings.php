5b6be582ff41325942f77ad8eaa1aa7e1de777e5
Mirko De Grandis <degami@gmail.com>
2025-01-26 23:50:09 +0100

    commit

--- app/site/graphql/Resolvers/Greetings.php
+++ app/site/graphql/Resolvers/Greetings.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * SiteBase
+ * PHP Version 8.3
+ *
+ * @category CMS / Framework
+ * @package  Degami\Sitebase
+ * @author   Mirko De Grandis <degami@github.com>
+ * @license  MIT https://opensource.org/licenses/mit-license.php
+ * @link     https://github.com/degami/sitebase
+ */
+
 namespace App\Site\GraphQL\Resolvers;
 
 use App\Base\Interfaces\GraphQl\ResolverInterface;

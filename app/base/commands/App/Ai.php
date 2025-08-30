6650ba2d860f93fd3144c1978d1e77d71c3e507c
Mirko De Grandis <degami@gmail.com>
2025-08-30 14:47:25 +0200

    commit

--- app/base/commands/App/Ai.php
+++ app/base/commands/App/Ai.php
@@ -19,7 +19,6 @@ use Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Terminal;
 

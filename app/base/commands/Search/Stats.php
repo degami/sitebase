c03cd7c9e9e26425a984929e720df0346f6c199d
Mirko De Grandis <degami@gmail.com>
2025-09-18 23:21:23 +0200

    added detail argumetns to search:stats cli command

--- app/base/commands/Search/Stats.php
+++ app/base/commands/Search/Stats.php
@@ -22,6 +22,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
 use HaydenPierce\ClassFinder\ClassFinder;
 use App\Base\Tools\Search\Manager as SearchManager;
+use Symfony\Component\Console\Input\InputArgument;
 
 /**
  * Cache Statistics Command
@@ -33,7 +34,9 @@ class Stats extends BaseCommand
      */
     protected function configure()
     {
-        $this->setDescription('Search Statistics');
+        $this->setDescription('Search Statistics')
+            ->addArgument('type', InputArgument::OPTIONAL, 'Type')
+            ->addArgument('id', InputArgument::OPTIONAL, 'Id');
     }
 
     /**
@@ -56,6 +59,9 @@ class Stats extends BaseCommand
 
             return Command::FAILURE;
         }
+
+        $showType = $input->getArgument('type');
+        $id = $input->getArgument('id');
 
         $count_result = $this->getSearch()->setQuery('*')->countAll();
 
@@ -83,14 +89,40 @@ class Stats extends BaseCommand
             }
         }
 
+        if ($showType && !isset($types[$showType])) {
+            $this->getIo()->error('Type not found');
+            return Command::FAILURE;
+        }
 
-        $tableContents = [
-            ["Total documents: " . $count_result],
-            ['<info>Type</info>', '<info>Count</info>']
-        ];
+        if ($showType && $id) {
+            $this->getSearch()->setQuery('type:"' . $showType . '" AND id:"' . $id . '"');
 
-        foreach ($types as $type => $count) {
-            $tableContents[] = [$type, $count];
+            if ($this->getSearch()->countAll() == 0) {
+                $this->getIo()->error('Element ' . $id . ' not found');
+                return Command::FAILURE;
+            }
+
+            foreach ($this->getSearch()->searchData(0, SearchManager::MAX_ELEMENTS_PER_QUERY)['docs'] as $doc) {
+                $this->getIo()->writeln(json_encode($doc));
+            }
+
+            return Command::SUCCESS;
+        } else if ($showType) {
+            $this->getSearch()->setQuery('type:"' . $showType . '"');
+
+            $tableContents = [['<info>id</info>','<info>website</info>','<info>locale</info>', '<info>title</info>']];
+            foreach ($this->getSearch()->searchData(0, SearchManager::MAX_ELEMENTS_PER_QUERY)['docs'] as $doc) {
+                $tableContents[] = [$doc['id'], $doc['website_id'], $doc['locale'], $doc['title']];
+            }
+        } else {
+            $tableContents = [
+                ["Total documents: " . $count_result],
+                ['<info>Type</info>', '<info>Count</info>']
+            ];
+
+            foreach ($types as $type => $count) {
+                $tableContents[] = [$type, $count];
+            }
         }
 
         $this->renderTitle('Search stats');

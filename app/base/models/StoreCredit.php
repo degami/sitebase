79c9ac25ed89500b03451dc4166d7b19c9ab96b6
Mirko De Grandis <degami@gmail.com>
2025-11-15 14:36:12 +0100

    dockblockr fixes

--- app/base/models/StoreCredit.php
+++ app/base/models/StoreCredit.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * SiteBase
+ * PHP Version 8.3
+ *
+ * @category CMS / Framework
+ * @package  Degami\Sitebase
+ * @author   Mirko De Grandis <degami@github.com>
+ * @license  MIT https://opensource.org/licenses/mit-license.php
+ * @link     https://github.com/degami/sitebase
+ */
+
+
 namespace App\Base\Models;
 
 use App\Base\Abstracts\Models\AccountModel;
